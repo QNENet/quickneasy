5cc49a8812fa7fd5724381156fb6dde5e4079680
paulf <paulf@qnenet.com>
2024-07-28 09:11:21 +1000

    chore: Update Vite and Spring Boot versions
    This commit updates the Vite and Spring Boot versions in the project. The Vite version is updated to the latest stable version, and the Spring Boot version is updated to 3.3.2. This ensures that the project is using the most up-to-date dependencies and takes advantage of any bug fixes or new features introduced in these versions.

--- src/main/java/com/qnenet/QuickNEasyApp.java
+++ src/main/java/com/qnenet/QuickNEasyApp.java
@@ -1,7 +1,10 @@
 package com.qnenet;
 
 import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.component.page.Push;
+import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.theme.lumo.Lumo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -12,12 +15,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * and some desktop browsers.
  *
  */
+@Push
 @SpringBootApplication
-@Theme(value = "quickneasy")
-public class Application implements AppShellConfigurator {
+@Theme(value = "quickneasy", variant = Lumo.DARK)
+@PWA(name = "QuickNEasy", shortName = "QNE", offlinePath = "offline.html", offlineResources = {
+        "images/offline.png" })
+public class QuickNEasyApp implements AppShellConfigurator {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(QuickNEasyApp.class, args);
     }
 
 }
--- src/main/java/com/qnenet/views/welcome/WelcomeView.java
+++ src/main/java/com/qnenet/views/welcome/WelcomeView.java
@@ -1,16 +1,19 @@
-package com.qnenet.views.empty;
+package com.qnenet.views.welcome;
 
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.H3;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
 import com.vaadin.flow.theme.lumo.LumoUtility.Margin;
 
-@PageTitle("Empty")
-@Route(value = "empty")
-public class EmptyView extends VerticalLayout {
+@PageTitle("Welcome")
+@Route(value = "")
+@RouteAlias(value = "")
+public class WelcomeView extends VerticalLayout {
 
-    public EmptyView() {
+    public WelcomeView() {
 
         setSizeFull();
         setSpacing(false);
@@ -18,9 +21,10 @@ public class EmptyView extends VerticalLayout {
         setDefaultHorizontalComponentAlignment(Alignment.CENTER);
         getStyle().set("text-align", "center");
 
-        H1 header = new H1("Empty");
+        H1 header = new H1("QuickNEasy");
         header.addClassNames(Margin.Top.XLARGE, Margin.Bottom.MEDIUM);
         add(header);
+        add(new H3("Software that enables the QNE Community"));
 
     }
 
