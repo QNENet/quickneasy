fc07c6a4702a282472ffd05e40db6f768568fe84
paulf <paulf@qnenet.com>
2024-07-29 08:03:48 +1000

    Refactor QuickNEasyApp main method for consistency

--- src/main/java/com/qnenet/QuickNEasyApp.java
+++ src/main/java/com/qnenet/QuickNEasyApp.java
@@ -23,7 +23,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class QuickNEasyApp implements AppShellConfigurator {
 
     public static void main(String[] args) {
-        SpringApplication.run(QuickNEasyApp.class, args);
+       SpringApplication.run(QuickNEasyApp.class, args);
     }
 
 }
